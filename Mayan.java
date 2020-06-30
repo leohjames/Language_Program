29bbd1ee47ef5df140d5c19d6ba9a0dabb6f3e0f
Leo James <66539145+leohjames@users.noreply.github.com>
2020-06-30 14:42:29 +0100

    Update Mayan.java

--- Mayan.java
+++ Mayan.java
@@ -1,9 +1,7 @@
 public class Mayan extends Language {
-
     public Mayan(String langName, int numSpeak) {
         super(langName, numSpeak, "Central America", "verb-object-subject");
     }
-
     @Override
     public void getInfo() {
         System.out.println(this.name + " is spoken by " + this.numSpeakers + " people mainly in " + this.regionsSpoken + ".\n" + "The language follows the word order: \n" + this.wordOrder);
